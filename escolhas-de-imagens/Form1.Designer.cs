2c14d99279d1cad332d4498cdad7f8f3c83d7df1
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-03-01 22:09:14 -0300

    Adicionar arquivos de projeto.

---
+++ escolhas-de-imagens/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace escolhas_de_imagens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureLivro3 = new System.Windows.Forms.PictureBox();
+            this.pictureLivro2 = new System.Windows.Forms.PictureBox();
+            this.pictureLivro1 = new System.Windows.Forms.PictureBox();
+            this.pictureNot3 = new System.Windows.Forms.PictureBox();
+            this.pictureNot2 = new System.Windows.Forms.PictureBox();
+            this.pictureNot1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLivro3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLivro2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLivro1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureNot3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureNot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureNot1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureLivro3
+            // 
+            this.pictureLivro3.Image = global::escolhas_de_imagens.Properties.Resources._61UHNHKpj_L__AC__SR360_460;
+            this.pictureLivro3.Location = new System.Drawing.Point(504, 371);
+            this.pictureLivro3.Name = "pictureLivro3";
+            this.pictureLivro3.Size = new System.Drawing.Size(193, 143);
+            this.pictureLivro3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureLivro3.TabIndex = 5;
+            this.pictureLivro3.TabStop = false;
+            this.pictureLivro3.Visible = false;
+            // 
+            // pictureLivro2
+            // 
+            this.pictureLivro2.Image = global::escolhas_de_imagens.Properties.Resources._71SYepTUsDL__AC__SR360_460;
+            this.pictureLivro2.Location = new System.Drawing.Point(504, 220);
+            this.pictureLivro2.Name = "pictureLivro2";
+            this.pictureLivro2.Size = new System.Drawing.Size(193, 133);
+            this.pictureLivro2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureLivro2.TabIndex = 4;
+            this.pictureLivro2.TabStop = false;
+            this.pictureLivro2.Visible = false;
+            // 
+            // pictureLivro1
+            // 
+            this.pictureLivro1.Image = global::escolhas_de_imagens.Properties.Resources._918Maoi6OML__AC__SR360_460;
+            this.pictureLivro1.Location = new System.Drawing.Point(504, 81);
+            this.pictureLivro1.Name = "pictureLivro1";
+            this.pictureLivro1.Size = new System.Drawing.Size(193, 130);
+            this.pictureLivro1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureLivro1.TabIndex = 3;
+            this.pictureLivro1.TabStop = false;
+            this.pictureLivro1.Visible = false;
+            // 
+            // pictureNot3
+            // 
+            this.pictureNot3.Image = global::escolhas_de_imagens.Properties.Resources._838914158;
+            this.pictureNot3.Location = new System.Drawing.Point(87, 371);
+            this.pictureNot3.Name = "pictureNot3";
+            this.pictureNot3.Size = new System.Drawing.Size(171, 143);
+            this.pictureNot3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureNot3.TabIndex = 2;
+            this.pictureNot3.TabStop = false;
+            this.pictureNot3.Visible = false;
+            // 
+            // pictureNot2
+            // 
+            this.pictureNot2.Image = global::escolhas_de_imagens.Properties.Resources.header_image;
+            this.pictureNot2.Location = new System.Drawing.Point(87, 220);
+            this.pictureNot2.Name = "pictureNot2";
+            this.pictureNot2.Size = new System.Drawing.Size(171, 133);
+            this.pictureNot2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureNot2.TabIndex = 1;
+            this.pictureNot2.TabStop = false;
+            this.pictureNot2.Visible = false;
+            // 
+            // pictureNot1
+            // 
+            this.pictureNot1.Image = global::escolhas_de_imagens.Properties.Resources._728118132;
+            this.pictureNot1.Location = new System.Drawing.Point(87, 81);
+            this.pictureNot1.Name = "pictureNot1";
+            this.pictureNot1.Size = new System.Drawing.Size(171, 130);
+            this.pictureNot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureNot1.TabIndex = 0;
+            this.pictureNot1.TabStop = false;
+            this.pictureNot1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.Location = new System.Drawing.Point(124, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Notebook";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(576, 47);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(54, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Livros";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox3.Location = new System.Drawing.Point(319, 156);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(79, 17);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "Notebook 1";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox4.Location = new System.Drawing.Point(319, 298);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(79, 17);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "Notebook 2";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(796, 139);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(58, 17);
+            this.checkBox6.TabIndex = 9;
+            this.checkBox6.Text = "Livro 1";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(796, 281);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(58, 17);
+            this.checkBox7.TabIndex = 9;
+            this.checkBox7.Text = "Livro 2";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(796, 451);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(58, 17);
+            this.checkBox8.TabIndex = 9;
+            this.checkBox8.Text = "Livro 3";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 542);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(504, 542);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 35);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox5.Location = new System.Drawing.Point(319, 441);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(79, 17);
+            this.checkBox5.TabIndex = 12;
+            this.checkBox5.Text = "Notebook 3";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(980, 589);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureLivro3);
+            this.Controls.Add(this.pictureLivro2);
+            this.Controls.Add(this.pictureLivro1);
+            this.Controls.Add(this.pictureNot3);
+            this.Controls.Add(this.pictureNot2);
+            this.Controls.Add(this.pictureNot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLivro3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLivro2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLivro1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureNot3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureNot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureNot1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureNot1;
+        private System.Windows.Forms.PictureBox pictureNot2;
+        private System.Windows.Forms.PictureBox pictureNot3;
+        private System.Windows.Forms.PictureBox pictureLivro1;
+        private System.Windows.Forms.PictureBox pictureLivro2;
+        private System.Windows.Forms.PictureBox pictureLivro3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox5;
+    }
+}
+
---
+++ escolhas-de-imagens/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace escolhas_de_imagens
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureNot2.Visible= false;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureNot1.Visible = true;
+            this.pictureNot2.Visible = true;
+            this.pictureNot3.Visible = true;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureLivro1.Visible = true;
+            this.pictureLivro2.Visible = true;
+            this.pictureLivro3.Visible = true;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureNot1.Visible = false;
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureNot3.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.pictureNot1.Visible = false;
+            this.pictureNot2.Visible = false;
+            this.pictureNot3.Visible = false;
+
+            this.pictureLivro1.Visible = false;
+            this.pictureLivro2.Visible = false;
+            this.pictureLivro3.Visible = false;
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureLivro1.Visible=true;
+           
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureLivro2.Visible = true;
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            this.pictureLivro3.Visible=true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
